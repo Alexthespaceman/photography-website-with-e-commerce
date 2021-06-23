f0965aa5b4b604c7e33538d9863cdd82a189e323
Alex Tristram <allex.tristram@gmail.com>
2021-06-23 12:09:02 +0100

    add footer with phone number link

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <div>
+        <a className="link number" href="tel:07801088105">
+          07801088105
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
