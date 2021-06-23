b7c431ed496ebb859cc61be8895b76347ea7843e
Alex Tristram <allex.tristram@gmail.com>
2021-06-23 12:08:37 +0100

    add links to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,27 @@
+import { Link } from "@reach/router";
 import React from "react";
 
 function Header(props) {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <Link className="link" to="/">
+        <div className="header-title">Alex Tristram Photography</div>
+      </Link>
+      <div className="navigation">
+        <ul className="list">
+          <Link className="link" to="">
+            <li className="list-child">Filter by country</li>
+          </Link>
+          <Link className="link" to="all-photos">
+            <li className="list-child">All Photographs</li>
+          </Link>
+          <Link className="link" to="contact-page">
+            <li className="list-child">Contact Alex</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
