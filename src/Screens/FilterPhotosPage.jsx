90b997cbc04bb73dfa0d7cfd1f56a7c488d5bde9
Alex Tristram <allex.tristram@gmail.com>
2021-06-24 10:41:34 +0100

    create filterPhotosPage

---
+++ src/Screens/FilterPhotosPage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FilterPhotosPage extends Component {
+  render() {
+    return <div>hello im filtered pics</div>;
+  }
+}
+
+export default FilterPhotosPage;
