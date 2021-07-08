2e30104fb63e81fa25ea910cba8d2f654669c3c3
alex tristram <tristram.alexander94@gmail.com>
2021-07-08 12:50:49 +0100

    add map to all photos page

--- src/Screens/AllPhotographsScreen.jsx
+++ src/Screens/AllPhotographsScreen.jsx
@@ -1,8 +1,407 @@
-import React, { Component } from "react";
+import { Component } from "react";
+import photo14 from "../Images/aeronol1.jpeg";
+import photo15 from "../Images/aeronol2.jpeg";
+import photo16 from "../Images/aeronol3.jpeg";
+import photo17 from "../Images/aeronol4.jpeg";
+import photo18 from "../Images/aeronol5.jpeg";
+import photo19 from "../Images/amadablam.jpeg";
+import photo3 from "../Images/bromo.jpeg";
+import photo22 from "../Images/chichenItza3.jpeg";
+import photo23 from "../Images/chichenIzta2.jpeg";
+import photo21 from "../Images/chithenItza.jpeg";
+import photo25 from "../Images/costaArrowFrog.jpeg";
+import photo1 from "../Images/costaBeach.jpeg";
+import photo24 from "../Images/costaBeach2.jpeg";
+import photo26 from "../Images/costaBirds.jpeg";
+import photo27 from "../Images/costaFrog.jpeg";
+import photo5 from "../Images/costaFrog2.jpeg";
+import photo28 from "../Images/costaSlothe2.jpeg";
+import photo6 from "../Images/costaSlothe3.jpeg";
+import photo29 from "../Images/costaStorm.jpeg";
+import photo30 from "../Images/costaTree.jpeg";
+import photo31 from "../Images/costSlothe.jpeg";
+import photo32 from "../Images/edinbourough.jpeg";
+import photo33 from "../Images/edinbourough2.jpeg";
+import { default as photo34, default as photo7 } from "../Images/glenCoe.jpeg";
+import photo35 from "../Images/highlandCattle.jpeg";
+import photo36 from "../Images/indainTea.jpeg";
+import photo11 from "../Images/indiaGoa.jpeg";
+import photo37 from "../Images/indiaGoaBeach.jpeg";
+import photo38 from "../Images/indiaGoaWaterfall.jpeg";
+import photo39 from "../Images/indiaHill.jpeg";
+import photo40 from "../Images/indiaHospital.jpeg";
+import photo12 from "../Images/indiajaipaw.jpeg";
+import photo41 from "../Images/indiaJaipaw3.jpeg";
+import photo42 from "../Images/indiaJaipaw4.jpeg";
+import photo43 from "../Images/indiaLuacknow.jpeg";
+import photo44 from "../Images/indianBombay.jpeg";
+import photo45 from "../Images/indianBombay2.jpeg";
+import photo8 from "../Images/indianBombayBoys.jpeg";
+import photo46 from "../Images/indianBus.jpeg";
+import photo47 from "../Images/indianClouds.jpeg";
+import photo48 from "../Images/indianDoctor.jpeg";
+import photo49 from "../Images/indianGirls.jpeg";
+import photo50 from "../Images/indianPalace.jpeg";
+import photo51 from "../Images/indianShow.jpeg";
+import photo52 from "../Images/indianUdipaw.jpeg";
+import photo53 from "../Images/indiaSunset.jpeg";
+import photo2 from "../Images/indiaSunset2.jpeg";
+import photo13 from "../Images/indiaTag.jpeg";
+import photo54 from "../Images/indiaTag2.jpeg";
+import photo55 from "../Images/indiaTag3.jpeg";
+import photo56 from "../Images/indiaTag4.jpeg";
+import photo57 from "../Images/indiaTucTuc.jpeg";
+import photo58 from "../Images/lakes.jpeg";
+import photo59 from "../Images/lakes1.jpeg";
+import photo10 from "../Images/lakes3.jpeg";
+import photo60 from "../Images/malamCove.jpeg";
+import photo4 from "../Images/mountains.jpeg";
+import photo61 from "../Images/pacificSea.jpeg";
+import photo62 from "../Images/scotland.jpeg";
+import photo9 from "../Images/scotlandBridge.jpeg";
+import photo63 from "../Images/scotlandRiver.jpeg";
+import photo64 from "../Images/udiapaw2.jpeg";
+import photo65 from "../Images/undipaw3.jpeg";
 
 class AllPhotographsScreen extends Component {
+  state = {
+    countries: [
+      {
+        country: "india",
+        image: [photo65],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo64],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo63],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo62],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo61],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo60],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo59],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo58],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo57],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo56],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo54],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo55],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo53],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo52],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo51],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo50],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo49],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo48],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo47],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo46],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo45],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo44],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo43],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo42],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo41],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo40],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo39],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo38],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo37],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo36],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo35],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo34],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo33],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo32],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo31],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo30],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo29],
+        description: "Beautiful",
+      },
+      {
+        country: "mexico",
+        image: [photo23],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo28],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo27],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo26],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo25],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo24],
+        description: "Beautiful",
+      },
+      {
+        country: "mexico",
+        image: [photo21],
+        description: "Beautiful",
+      },
+      {
+        country: "mexico",
+        image: [photo22],
+        description: "Beautiful",
+      },
+      {
+        country: "Nepal",
+        image: [photo19],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo17],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo18],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo16],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo15],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo14],
+        description: "Beautiful",
+      },
+      {
+        country: "indonesia",
+        image: [photo3],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo11],
+        description: "Beautiful",
+      },
+      {
+        country: "india",
+        image: [photo12],
+        description: "stunning",
+      },
+      {
+        country: "india",
+        image: [photo8],
+        description: "wow",
+      },
+      {
+        country: "india",
+        image: [photo2],
+        description: "so cool",
+      },
+      {
+        country: "india",
+        image: [photo13],
+        description: "so cool",
+      },
+      {
+        country: "UK",
+        image: [photo7],
+        description: "Beautiful",
+      },
+      {
+        country: "UK",
+        image: [photo10],
+        description: "crazy cool",
+      },
+      {
+        country: "UK",
+        image: [photo9],
+        description: "jhee whizz",
+      },
+      {
+        country: "costa",
+        image: [photo5],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo6],
+        description: "Beautiful",
+      },
+      {
+        country: "costa",
+        image: [photo1],
+        description: "Beautiful",
+      },
+      {
+        country: "Nepal",
+        image: [photo4],
+        description: "Beautiful",
+      },
+    ],
+  };
+
   render() {
-    return <div className="all-photos"></div>;
+    return (
+      <div className="filtered-photos-title">
+        {this.state.countries.map((place) => {
+          return (
+            <div className="filtered-photo-box">
+              <img className="photo-frame" src={place.image}></img>
+              <p className="filtered-desription">{place.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
